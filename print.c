3aa98688f78443bec2cf6e084766839c29987364
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 22:36:55 +0300

    Update print.c

--- print.c
+++ print.c
@@ -1,26 +1,43 @@
 #include "main.h"
 /**
-* _printf - custom printf version
-* @format: initial string with all identifiers
-* Return: number of characters printed
-*/
+ * _printf - custom printf version
+ * @format: initial string with all identifiers
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int total_len = 0, i = 0;
+	int total_len = 0, i;
 	va_list list;
-	int (*function)(va_list, flags_t *);
-	flags_t flags = {0, 0, 0};
-
+	int (*function)(va_list);
 	
 	va_start(list, format);
 	
 	if (format == NULL)
 		return (-1);
-	
-	if (!format[i])
-		return (0);
-	while (format[i])
-	{
-	}
 
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{	total_len += _putchar(format[i]);
+				i++;
+			}
+			else if (format[i + 1] != '\0')
+			{
+				function = get_func(format[i + 1]);
+				total_len += (function ? function(list) : _putchar(format[i]) + _putchar(format[i + 1]));
+				i++;
+				
+			}
+
+		}
+		else
+			total_len += _putchar(format[i]);
+
+	}
+	va_end(list);
+	return (total_len);
+	
+	
 }
