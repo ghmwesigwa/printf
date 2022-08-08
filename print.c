f226217a8714294585dcb9f686b39b318b2f525e
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 00:22:25 +0300

    Update print.c

--- print.c
+++ print.c
@@ -1,47 +1,43 @@
 #include "main.h"
 
 /**
-* _printf - custom printf version
-* @format: initial string with all identifiers
-* Return: number of characters printed
-**/
+ * _printf - custom printf version
+ * @format: initial string with all identifiers
+ * Return: number of characters printed
+ **/
 int _printf(const char *format, ...)
 {
-	int total_len = 0;
-	int i;
-	va_list list;
-	int (*function)(va_list);
-	
-	va_start(list, format);
-	
-	if (format == NULL)
-		return (-1);
+    int total_len = 0;
+    int i;
+    va_list list;
+    int (*function)(va_list);
 
-	for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '%')
-			{	total_len += _write_char(format[i]);
-				i++;
-			}
-			else if (format[i + 1] != '\0')
-			{
-				function = picker(format[i + 1]);
-				total_len += (function ? function(list) : _write_char(format[i]) + _write_char(format[i + 1]));
-				i++;
-				
-			}
+    va_start(list, format);
 
-		}
-		else
-		{
-			total_len += _write_char(format[i]);
-		}
+    if (format == NULL)
+        return (-1);
 
-	}
-	va_end(list);
-	return (total_len);
-	
-	
+    for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == '%')
+            {
+                total_len += _write_char(format[i]);
+                i++;
+            }
+            else if (format[i + 1] != '\0')
+            {
+                function = picker(format[i + 1]);
+                total_len += (function ? function(list) : _write_char(format[i]) + _write_char(format[i + 1]));
+                i++;
+            }
+        }
+        else
+        {
+            total_len += _write_char(format[i]);
+        }
+    }
+    va_end(list);
+    return (total_len);
 }
