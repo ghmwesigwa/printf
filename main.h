096c48511ac551f790b9018483004b456f985513
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 13:25:52 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h> /* va_list */
+#include <stddef.h> /* typedef */
+#include <unistd.h> /* write */
+#include <stdio.h>  /* malloc, free */
+#include <stdlib.h> /* memory allocation, process control, conversions, etc */
 
+/* printf functions */
 int _printf(const char *format, ...);
 
 #endif
