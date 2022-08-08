38f2617a2ecb019e79d69c81a8c45e0dd1ed0553
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 22:59:03 +0300

    Create pick.c

---
+++ pick.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* pick - search and return the right print_ function
+* @s: array
+* Return: return a function
+**/
+
+int i;
+
+int (*pick(char s))(va_list)
+{
+	op_t operations[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+    {"b", print_binary},
+    {"r", print_reversed},
+    {"R", rot13},
+    {"u", unsigned_integer},
+    {"o", print_octal},
+    {"x", print_hex},
+    {"X", print_heX},
+		{NULL, NULL}
+	};
+  
+  for (i = 0; operations[i].c != NULL; i++)
+	{
+		if (*operations[i].c == s)
+			return (operations[i].f);
+	}
+	return (NULL);
+}
