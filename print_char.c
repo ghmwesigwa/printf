a7623e65770728f0c8cfaa51159bfea10c344da0
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 01:50:24 +0300

    project files

---
+++ print_char.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_char - returns the character
+ * @buff_dest: buffer
+ * @arg: list of arguments
+ * @buff_count: index of buffer pointer
+ * Return: buffer index
+ */
+int print_char(char *buff_dest, va_list arg, int buff_count)
+{
+	char c = va_arg(arg, int);
+
+	buff_dest[buff_count] = c;
+
+	return (++buff_count);
+}
