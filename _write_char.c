f7602a0318f6981a32d3852b25ce40a1e84c4987
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 23:46:16 +0300

    Update _write_char.c

--- _write_char.c
+++ _write_char.c
@@ -1,1 +1,13 @@
+#include <unistd.h>
+
+/**
+* _write_char - writes the character c to stdout
+* @c: character to be  printed
+* Return: 1 (on success),
+*	 -1 (on error)
+*/
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
 
