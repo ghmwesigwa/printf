ad7a6d5fbc769d1fe32a02d09deb36161de798ec
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 20:01:36 +0300

    Update print.c

--- print.c
+++ print.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _printf - custom printf version
- * @format: initial string with all identifiers
- * Return: number of characters printed
- */
+* _printf - custom printf version
+* @format: initial string with all identifiers
+* Return: number of characters printed
+*/
 int _printf(const char *format, ...)
 {
 	int total_len = 0, i = 0;
