bcb82cd7c30f392aafacb674a7f7ba0e3e3fb170
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 01:42:56 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define INT_BITS (4 * 8)
 
 #include <stdarg.h> /* va_list */
 #include <stddef.h> /* typedef */
 #include <unistd.h> /* write */
 #include <stdio.h> /* malloc, free */
 #include <stdlib.h> /* memory allocation, process control, conversions, etc */
-
-/* printf functions */
-int _printf(const char *format, ...);
+#include <limits.h>
 
 /**
 *struct func_identifier - struct
@@ -17,8 +16,29 @@ int _printf(const char *format, ...);
 **/
 typedef struct func_identifier
 {
-	char *c;
-	int (*f)(va_list);
+	char t;
+	int (*f)(char *, va_list, int);
 } func_id;
+
+/* helper functions */
+int buff_append(char *buff_dest, va_list arg, int buff_count, char type);
+int print_buff(char *buff, unsigned int nbuff);
+int str_len(char *s);
+char *_strcpy(char *dest, char *src);
+
+/* printf functions */
+int _printf(const char *format, ...);
+int print_char(char *buff_dest, va_list arg, int buff_count);
+int print_str(char *buff_dest, va_list arg, int buff_count);
+int print_int(char *buff_dest, va_list list, int buff_count);
+int print_perc(char *buff_dest, va_list arg, int buff_count);
+int print_bin(char *buff_dest, va_list arg, int buff_count);
+int print_oct(char *buff_dest, va_list arg, int buff_count);
+int print_hex(char *buff_dest, va_list arg, int buff_count);
+int print_X(char *buff_dest, va_list arg, int buff_count);
+int print_uint(char *buff_dest, va_list arg, int buff_count);
+int print_rev(char *buff_dest, va_list arg, int buff_count);
+int print_R13(char *buff_dest, va_list arg, int buff_count);
+
 
 #endif
