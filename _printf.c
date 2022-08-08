5feee05eb95855f697c3d8e3cfdeba649f3de5d6
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 01:00:27 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,44 +1,47 @@
 #include "main.h"
-#include "picker.c"
-
 /**
- * _printf - custom printf version
- * @format: initial string with all identifiers
- * Return: number of characters printed
- **/
+* _printf - main function to print in console
+* @format: array to print and check type
+* Return: count of character printed
+**/
 int _printf(const char *format, ...)
 {
-    int total_len = 0;
-    int i;
-    va_list arg_list;
-    int (*function)(va_list);
+	int count = -1;
 
-    va_start(arg_list, format);
+	if (format != NULL)
+	{
+		int i;
+		va_list ar_list;
+		int (*o)(va_list);
 
-    if (format == NULL)
-        return (-1);
+		va_start(ar_list, format);
 
-    for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == '%')
-            {
-                total_len += _write_char(format[i]);
-                i++;
-            }
-            else if (format[i + 1] != '\0')
-            {
-                function = picker(format[i + 1]);
-                total_len += (function ? function(arg_list) : _write_char(format[i]) + _write_char(format[i + 1]));
-                i++;
-            }
-        }
-        else
-        {
-            total_len += _write_char(format[i]);
-        }
-    }
-    va_end(arg_list);
-    return (total_len);
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+
+		count = 0;
+
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					o = get_func(format[i + 1]);
+					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
+					i++;
+				}
+			}
+			else
+				count += _putchar(format[i]);
+		}
+		va_end(ar_list);
+	}
+
+	return (count);
 }
