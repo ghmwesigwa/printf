4719bfb26a08ee0273bbded5d03e97a8988b19c5
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 00:11:59 +0300

    Update _write_char.c

--- _write_char.c
+++ _write_char.c
@@ -3,9 +3,8 @@
 /**
 * _write_char - writes the character c to stdout
 * @c: character to be  printed
-* Return: 1 (on success),
-*	 -1 (on error)
-*/
+* Return: 1 (on success), -1 (on error)
+**/
 int _write_char(char c)
 {
 	return (write(1, &c, 1));
