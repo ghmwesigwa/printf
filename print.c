75c0c3f8ff24439a854bee400c07810b4441feec
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 18:13:03 +0300

    Update print.c

--- print.c
+++ print.c
@@ -6,13 +6,17 @@
  */
 int _printf(const char *format, ...)
 {
-	int total_len = 0;
+	int total_len = 0, i = 0;
 	va_list list;
 	char *p, *start;
-	params_t params = PARAMS_INIT;
 	
 	va_start(list, format);
 	
+	if (format == NULL)
+		return (-1);
+	
+	if (!format[i])
+		return (0);
 	
   
   
