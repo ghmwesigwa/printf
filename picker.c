2206117ff284c8027fd668fc9a160aa6e012e413
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 23:23:28 +0300

    Update picker.c

--- picker.c
+++ picker.c
@@ -15,13 +15,13 @@ int (*picker(char s))(va_list)
 		{"%", print_percent},
 		{"d", print_integer},
 		{"i", print_integer},
-    {"b", print_binary},
-    {"r", print_reversed},
-    {"R", rot13},
-    {"u", unsigned_integer},
-    {"o", print_octal},
-    {"x", print_hex},
-    {"X", print_heX},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
 		{NULL, NULL}
 	};
   
