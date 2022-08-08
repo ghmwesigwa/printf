d3491c454334d70e435f2d3a96af1a8706bfb0e0
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-08 19:53:50 +0300

    Update print.c

--- print.c
+++ print.c
@@ -8,22 +8,37 @@ int _printf(const char *format, ...)
 {
 	int total_len = 0, i = 0;
 	va_list list;
-	char *buffer, *start;
-	int (*function)(va_list);
+	int (*function)(va_list, flags_t *);
+	flags_t flags = {0, 0, 0};
+
 	
-	va_start(list, format), buffer = malloc(sizeof(char) * 1024);;
+	va_start(list, format);
 	
 	if (format == NULL)
 		return (-1);
 	
 	if (!format[i])
 		return (0);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{	total_len += _putchar('%');
+				continue;
+			}
+			while (get_flag(format[i], &flags))
+				format[i + 1];
+			function = get_print(format[i]);
+			total_len += (function) ? function(list, &flags) : _printf("%%%c", format[i]);
+		}
+		else
+			total_len += _putchar(format[i]);
+		i++;
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (total_len);
 	
 	
-	
-	
-	
-	
-  
-  
 }
