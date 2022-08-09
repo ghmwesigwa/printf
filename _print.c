3f9da0453c3a19641440ac78b0d7bdc6869c733d
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 17:07:54 +0300

    Update _print.c

--- _print.c
+++ _print.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+* func_picker - search and return the right print_ function
+* @s: array
+* Return: return a function
+**/
+
+int i;
+
+int (*func_picker(char s))(va_list)
+{
+	func_id f_list[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_p}
+
+	};
+	
+	for (i = 0; f_list[i].c != NULL; i++)
+	{
+		if (*f_list[i].c == s)
+			return (f_list[i].f);
+	}
+	return (NULL);
+}
+
+/**
  * _printf - custom printf version
  * @format: initial string with all identifiers
  * Return: number of characters printed
@@ -28,7 +53,7 @@ int _printf(const char *format, ...)
             }
             else if (format[i + 1] != '\0')
             {
-                function = picker(format[i + 1]);
+                function = func_picker(format[i + 1]);
                 total_len += (function ? function(arg_list) : _write_char(format[i]) + _write_char(format[i + 1]));
                 i++;
             }
