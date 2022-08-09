96de9ad2f3758c08a43034f1a00d3c8c3306974b
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 17:12:45 +0300

    Create _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+include "main.h"
+  
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: number of printed char
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
