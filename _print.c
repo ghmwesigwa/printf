0a0daab97f1c9ec4e9d1a6d21215e5b442d47e8d
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 21:25:00 +0300

    Update _print.c

--- _print.c
+++ _print.c
@@ -32,8 +32,8 @@ int (*func_picker(char s))(va_list)
  **/
 int _printf(const char *format, ...)
 {
-
-	return (-1);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
 
 	for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
 	{
