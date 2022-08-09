1a21c5b67d95a6550c60df7dde4568d2ac313fd8
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 23:19:39 +0300

    Update _print.c

--- _print.c
+++ _print.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * func_picker - search and return the right print_ function
- * @s: array
- * Return: return a function
- **/
+* func_picker - search and return the right print_ function
+* @s: array
+* Return: return a function
+**/
+
 
 int (*func_picker(char s))(va_list)
 {
-    func_id f_list[] = {
-        {"c", print_c},
-        {"s", print_s},
-        {"%", print_p}
-    };
-    int i;
+	func_id f_list[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_p}
 
-    for (i = 0; f_list[i].c != NULL; i++)
-    {
-        if (*f_list[i].c == s)
-            return (f_list[i].f);
-    }
-    return (NULL);
+	};
+	int i;
+
+	for (i = 0; f_list[i].c != NULL; i++)
+	{
+		if (*f_list[i].c == s)
+			return (f_list[i].f);
+	}
+	return (NULL);
 }
 
 /**
@@ -30,39 +32,36 @@ int (*func_picker(char s))(va_list)
  **/
 int _printf(const char *format, ...)
 {
-    int i, total_len = 0;
-    va_list arg_list;
-    int (*function)(va_list);
+	int i, total_len = 0;
+	va_list arg_list;
+	int (*function)(va_list);
 
-    if (format != NULL)
-    {
+	va_start(arg_list, format);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
 
-        va_start(arg_list, format);
-        if (format[0] == '%' && format[1] == '\0')
-            return (-1);
-
-        for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
-        {
-            if (format[i] == '%')
-            {
-                if (format[i + 1] == '%')
-                {
-                    total_len += _putchar(format[i]);
-                    i++;
-                }
-                else if (format[i + 1] != '\0')
-                {
-                    function = func_picker(format[i + 1]);
-                    total_len += (function ? function(arg_list) : _putchar(format[i]) + _putchar(format[i + 1]));
-                    i++;
-                }
-            }
-            else
-            {
-                total_len += _putchar(format[i]);
-            }
-        }
-        va_end(arg_list);
-    }
-    return (total_len);
+	for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				total_len += _putchar(format[i]);
+				i++;
+			}
+			else if (format[i + 1] != '\0')
+			{
+				function = func_picker(format[i + 1]);
+				total_len += (function ? function(arg_list) :
+					      _putchar(format[i]) + _putchar(format[i + 1]));
+				i++;
+			}
+		}
+		else
+		{
+			total_len += _putchar(format[i]);
+		}
+	}
+	va_end(arg_list);
+	return (total_len);
 }
