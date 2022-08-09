e8b545e816e4d0481ac55d3218dcee54200e28df
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 16:34:28 +0300

    Create print_char.c

---
+++ print_char.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_c - print single character
+ * @ap: arg list
+ * Return: number of printed char
+ */
+int print_c(va_list list)
+{
+	char c = va_arg(list, int);
+
+	if (c == '\0')
+		return (write(1, &c, 1));
+
+	_putchar(c);
+	return (1);
+}
