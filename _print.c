2e13a0b0ceabf417969f6d62137337b1f42d0a43
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 17:23:27 +0300

    Update _print.c

--- _print.c
+++ _print.c
@@ -39,8 +39,8 @@ int _printf(const char *format, ...)
 
     va_start(arg_list, format);
 
-    if (format == NULL)
-        return (-1);
+    if (format[0] == '%' && format[1] == '\0')
+	    return (-1);
 
     for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
     {
