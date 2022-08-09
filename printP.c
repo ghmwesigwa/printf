ef7641f3af039f5d2aa2a54bea77be45055f2294
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 21:43:23 +0300

    Update printP.c

--- printP.c
+++ printP.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_percent -  print percent character
+ * print_p -  print percent character
  * @args: char
  * Return: percent character
  */
 
-int print_percent(va_list args __attribute__((unused)))
+int print_p(va_list args __attribute__((unused)))
 {
 	return (_putchar('%'));
 }
