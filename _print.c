ff7429972a503109966e58d02979f9fe09060388
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 20:39:22 +0300

    fixed indentation and white space

--- _print.c
+++ _print.c
@@ -33,36 +33,36 @@ int (*func_picker(char s))(va_list)
 int _printf(const char *format, ...)
 {
 	int total_len = 0;
-    int i;
-    va_list arg_list;
-    int (*function)(va_list);
-
-    va_start(arg_list, format);
-
-    if (format[0] == '%' && format[1] == '\0')
-	    return (-1);
-
-    for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == '%')
-            {
-                total_len += _putchar(format[i]);
-                i++;
-            }
-            else if (format[i + 1] != '\0')
-            {
-                function = func_picker(format[i + 1]);
-                total_len += (function ? function(arg_list) : _putchar(format[i]) + _putchar(format[i + 1]));
-                i++;
-            }
-        }
-        else
-        {
-            total_len += _putchar(format[i]);
-        }
-    }
-    va_end(arg_list);
-    return (total_len);
+	int i;
+	va_list arg_list;
+	int (*function)(va_list);
+	
+	va_start(arg_list, format);
+	
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+	
+	for (i = 0; format[i] != '\0'; i++) /* Iterates through the main string*/
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				total_len += _putchar(format[i]);
+				i++;
+			}
+			else if (format[i + 1] != '\0')
+			{
+				function = func_picker(format[i + 1]);
+				total_len += (function ? function(arg_list) : _putchar(format[i]) + _putchar(format[i + 1]));
+				i++;
+			}
+		}
+		else
+		{
+			total_len += _putchar(format[i]);
+		}
+	}
+	va_end(arg_list);
+	return (total_len);
 }
