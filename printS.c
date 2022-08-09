3404cf866209085c08dd253737ae68fe2f5c3cfb
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-09 16:49:51 +0300

    Create printS.c

---
+++ printS.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+ * print_s - print a string
+ * @ap: arg list
+ * Return: number of printed char
+ */
+int print_s(va_list list)
+{
+	char *arg = va_arg(list, char *);
+	int sum = 0;
+
+	if (!arg)
+	{
+		sum += _puts("(null)", 0);
+		return (sum);
+	}
+	return (_puts(arg, 0));
+}
